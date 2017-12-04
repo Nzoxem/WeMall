3ca39d005f6ec2bf3ec63aa1fd0318edbf22f290
eris <zxeris@foxmail.com>
2017-12-04 11:18:13 +0800

    选择收货地址

--- pages/seladdress/seladdress.js
+++ pages/seladdress/seladdress.js
@@ -1,66 +1,90 @@
-// pages/seladdress/seladdress.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    addressList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  selectTap: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.request({
+      url: app.globalData.subDomain + "/user/shipping-address/update",
+      data: {
+        id: id,
+        token: app.globalData.token,
+        isDefault: "true"
+      },
+      success: function (res) {
+        wx.navigateBack({});
+      },
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  editAddress: function (e) {
+    wx.navigateTo({
+      url: "/pages/addaddress/addaddress?id=" + e.currentTarget.dataset.id,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  addAddress: function (e) {
+    wx.navigateTo({
+      url: "/pages/addaddress/addaddress",
+    });
+  },
   onShow: function () {
-  
+    var that = this;
+    wx.request({
+      url: app.globalData.subDomain + "/user/shipping-address/list",
+      data: {
+        token: app.globalData.token
+      },
+      success: function (res) {
+        if(res.data.code==0){
+          that.setData({
+            addressList:res.data.data
+          });
+        }else if(res.data.code==700){
+          that.setData({
+            addressList:null
+          });
+        }
+       
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
